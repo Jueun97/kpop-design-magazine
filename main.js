643e4f840474121a49f048cf3ad076766b1ef273
Jueun <zxnm0901@naver.com>
2021-06-02 19:00:42 +0900

    contact link

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ const header = document.querySelector('.header');
 const headerIndexButton = document.querySelector('.header-index');
 const index = document.querySelector('#index');
 const indexButton = document.querySelector('.index-arrow');
+const contactIndex = document.querySelector('.contact-index');
 let scrollHeight = 0;
-headerIndexButton.addEventListener('click', () => {
+function indexHandler() {
     body.style.overflow = 'hidden';
     header.style.display = 'none';
     window.scrollTo(0, 0);
     index.style.top = '0';
+}
+headerIndexButton.addEventListener('click', () => {
+    indexHandler();
 })
 indexButton.addEventListener('click', () => {
     index.style.top = '-1000px';
     body.style.overflow = 'inherit';
     header.style.display = 'flex';
 })
-/* window.addEventListener('scroll', (event) => {
-    scrollHeight = window.scrollY;
-    console.log(scrollHeight)
-
-}) */
+contactIndex.addEventListener('click', () => {
+    indexHandler();
+})
