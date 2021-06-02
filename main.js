6bef6fdc3fcfb14effd86bfe1fd5b80ad2ebed56
Jueun <zxnm0901@naver.com>
2021-06-02 23:09:41 +0900

    intro animation

--- main.js
+++ main.js
@@ -22,3 +22,26 @@ indexButton.addEventListener('click', () => {
 contactIndex.addEventListener('click', () => {
     indexHandler();
 })
+
+const intro = document.querySelector('#intro-main3');
+const main4Image1 = document.querySelector('.main4-image-1');
+const main4Image2 = document.querySelector('.main4-image-2');
+const main4Image3 = document.querySelector('.main4-image-3');
+let check = true;
+window.addEventListener('scroll', (event) => {
+      const base = window.screen.height * 3 - 100 ;
+    if (base < window.scrollY && check) {
+        check = false;
+        main4Image1.style.top = '50px';
+        main4Image2.style.top = '-120px';
+        main4Image2.style.left = '43%';
+        main4Image3.style.left = '-50%';
+    }
+    else if (base > window.scrollY) {
+        main4Image1.style.top = '200px';
+        main4Image2.style.top = '-100px';
+        main4Image2.style.left = '-250px';
+        main4Image3.style.left = '270px'; 
+        check = true;
+    } 
+})
