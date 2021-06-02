9240b5bd01f469c9d607883b19f8bd6205e385fd
Jueun <zxnm0901@naver.com>
2021-06-02 23:29:33 +0900

    intro, trend2 animation

--- main.js
+++ main.js
@@ -27,21 +27,40 @@ const intro = document.querySelector('#intro-main3');
 const main4Image1 = document.querySelector('.main4-image-1');
 const main4Image2 = document.querySelector('.main4-image-2');
 const main4Image3 = document.querySelector('.main4-image-3');
-let check = true;
+const simpleImage = document.querySelector('#simple');
+const identityImage = document.querySelector('#identity');
+const flexibleImage = document.querySelector('#flexible');
+let checkForIntro = true;
+let checkForTrend = true;
 window.addEventListener('scroll', (event) => {
-      const base = window.screen.height * 3 - 100 ;
-    if (base < window.scrollY && check) {
-        check = false;
+    const baseForIntro = window.screen.height * 3 - 100 ;
+    if (main4Image1 && baseForIntro < window.scrollY && checkForIntro) {
+        checkForIntro = false;
         main4Image1.style.top = '50px';
         main4Image2.style.top = '-120px';
         main4Image2.style.left = '43%';
         main4Image3.style.left = '-50%';
-    }
-    else if (base > window.scrollY) {
+    } else if (main4Image1 && baseForIntro > window.scrollY) {
         main4Image1.style.top = '200px';
         main4Image2.style.top = '-100px';
         main4Image2.style.left = '-250px';
         main4Image3.style.left = '270px'; 
-        check = true;
-    } 
+        checkForIntro = true;
+    }
+    const baseForTrend = window.screen.height - 100;
+    if (simpleImage && baseForTrend <= window.scrollY && checkForTrend) {
+        console.log("hello")
+        checkForTrend = false;
+        simpleImage.style.right = '0px';
+        flexibleImage.style.right = '0px';
+        identityImage.style.left = '0px';
+    } else if (simpleImage && window.scrollY < 100) {
+        checkForTrend = true;
+        simpleImage.style.right = '40%';
+        flexibleImage.style.right = '40%';
+        identityImage.style.left = '40%';
+    }
+    
+    
+    
 })
